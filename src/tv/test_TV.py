603c5eadcb361f86a894d595178aaa5e9b1afd91
Joel Almeida <joel.almeida.sa@gmail.com>
2022-08-08 19:21:54 -0300

    test(TV): test turn_up_volume method.

--- src/tv/test_TV.py
+++ src/tv/test_TV.py
@@ -8,3 +8,12 @@ def test_initial_atributes():
     assert tv.volume == 50
     assert tv.on is False
     assert tv.screen_size == 14
+
+
+def test_turn_up_volume():
+    tv.turn_up_volume()
+    assert tv.volume == 51
+
+    for i in range(50):
+        tv.turn_up_volume()
+    assert tv.volume == 99
