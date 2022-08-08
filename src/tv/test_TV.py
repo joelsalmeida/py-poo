b6a2ee48a4a49766b675c92961f0c06871804fc6
Joel Almeida <joel.almeida.sa@gmail.com>
2022-08-08 20:21:47 -0300

    test(TV): test change_chanel method.

--- src/tv/test_TV.py
+++ src/tv/test_TV.py
@@ -1,3 +1,5 @@
+import pytest
+
 from TV import TV
 
 tv = TV(14)
@@ -26,3 +28,14 @@ def test_turn_down_volume():
     for i in range(100):
         tv.turn_down_volume()
     assert tv.volume == 0
+
+
+def test_change_chanel():
+    with pytest.raises(ValueError):
+        tv.change_chanel(100)
+
+    with pytest.raises(ValueError):
+        tv.change_chanel(-1)
+
+    tv.change_chanel(7)
+    assert tv.chanel == 7
