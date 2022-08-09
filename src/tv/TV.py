1d3fd6f032896c8f21fdfd430aae222ab4c74d62
Joel Almeida <joel.almeida.sa@gmail.com>
2022-08-08 21:46:27 -0300

    feat/refactoring: implements turn_on_off method. update methods to "raise" when tv is off on some invalid interaction

--- src/tv/TV.py
+++ src/tv/TV.py
@@ -21,20 +21,29 @@ class TV:
     def on(self):
         return self.__on
 
-    @on.setter
-    def on(self, on_status):
-        self.__on = on_status
-
     def turn_up_volume(self):
+        self.rais_if_off()
         if self.__volume < 99:
             self.__volume += 1
 
     def turn_down_volume(self):
+        self.rais_if_off()
         if self.__volume > 0:
             self.__volume -= 1
 
     def change_chanel(self, chanel):
+        self.rais_if_off()
         if chanel > 99 or chanel < 0:
             raise ValueError('Invalid chanel')
 
         self.__chanel = chanel
+
+    def turn_on_off(self):
+        if self.__on is True:
+            self.__on = False
+        else:
+            self.__on = True
+
+    def rais_if_off(self):
+        if self.__on is False:
+            raise ValueError('TV is off')
