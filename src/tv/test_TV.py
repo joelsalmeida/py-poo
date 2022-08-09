6c88e704a90cbdc3cd8e74e293704a9c7e9c129a
Joel Almeida <joel.almeida.sa@gmail.com>
2022-08-08 21:41:43 -0300

    test(TV) test turn_on_off method. test if "turn_up_volume, turn_down_volume, change_chanel" when off raises ValueError

--- src/tv/test_TV.py
+++ src/tv/test_TV.py
@@ -2,40 +2,65 @@ import pytest
 
 from TV import TV
 
-tv = TV(14)
-
 
 def test_initial_atributes():
+    tv = TV(14)
+
     assert tv.on is False
     assert tv.volume == 50
     assert tv.on is False
     assert tv.screen_size == 14
 
 
+def test_raises_if_invalid_interactions_when_off():
+    tv_off = TV(14)
+
+    with pytest.raises(ValueError):
+        tv_off.turn_up_volume()
+
+    with pytest.raises(ValueError):
+        tv_off.turn_down_volume()
+
+    with pytest.raises(ValueError):
+        tv_off.change_chanel(7)
+
+
+tv_on = TV(14)
+tv_on.turn_on_off()  # NOW, TV IS ON
+
+
 def test_turn_up_volume():
-    tv.turn_up_volume()
-    assert tv.volume == 51
+    tv_on.turn_up_volume()
+    assert tv_on.volume == 51
 
     for i in range(50):
-        tv.turn_up_volume()
-    assert tv.volume == 99
+        tv_on.turn_up_volume()
+    assert tv_on.volume == 99
 
 
 def test_turn_down_volume():
-    tv.turn_down_volume()
-    assert tv.volume == 98
+    tv_on.turn_down_volume()
+    assert tv_on.volume == 98
 
     for i in range(100):
-        tv.turn_down_volume()
-    assert tv.volume == 0
+        tv_on.turn_down_volume()
+    assert tv_on.volume == 0
 
 
 def test_change_chanel():
     with pytest.raises(ValueError):
-        tv.change_chanel(100)
+        tv_on.change_chanel(100)
 
     with pytest.raises(ValueError):
-        tv.change_chanel(-1)
+        tv_on.change_chanel(-1)
 
-    tv.change_chanel(7)
-    assert tv.chanel == 7
+    tv_on.change_chanel(7)
+    assert tv_on.chanel == 7
+
+
+def test_turn_on_off():
+    tv_on.turn_on_off()
+    assert tv_on.on is False
+
+    tv_on.turn_on_off()
+    assert tv_on.on is True
